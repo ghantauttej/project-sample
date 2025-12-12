f66cbd8056bdc5de77dfaeac92e7783e43113b8c
uttej <uttejmulticloud@gmail.com>
2025-12-12 07:20:52 +0530

    terraform-updated-2

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "one" {
-  count                  = 2
+  count                  = 3
   ami                    = "ami-0ecb62995f68bb549"
   instance_type          = "c7i-flex.large"
   key_name               = "Swiggy"
@@ -14,5 +14,5 @@ resource "aws_instance" "one" {
 }
 
 variable "instance_names" {
-  default = ["tomcat-1", "Monitoring server"]
+  default = ["tomcat-1","tomcat-2","Monitoring server"]
 }
